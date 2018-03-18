8974c7a68a5a88c3707ca4574eca87643e76cc4d
Judah Schwartz <judahsch@gmail.com>
2018-03-18 16:25:09 -0400

    Major refactor to meet requirements

--- Program.cs
+++ Program.cs
@@ -10,22 +10,22 @@ namespace Anagram_Finder
 {
     class Program
     {
-        static readonly HashSet<AnagrammizedWord> Words = new HashSet<AnagrammizedWord>();
-
+        private static readonly AnnagramBucketSet AnnagramBucketSet = new AnnagramBucketSet();
+        
         static void Main(string[] args)
         {
             ReadFromFile();
-            var orderedByAnagram = Words.OrderBy(aw => aw.Anagram).ToArray();
-            string currentAnagram = String.Empty;
-            foreach (var word in orderedByAnagram)
+            var topAnnagramBuckets = AnnagramBucketSet.GetTopAnnagramBuckets(10);
+            int count = 0;
+            foreach (var bucket in topAnnagramBuckets)
             {
-                if (word.Anagram != currentAnagram)
+                Console.WriteLine($"Number {++count}: Annagram Group:{bucket.Annagram} Number of Annagrams: {bucket.AnnagramCount}");
+                foreach (var word in bucket.WordSet)
                 {
-                    currentAnagram = word.Anagram;
-                    Console.WriteLine("__________________________________");
+                    Console.WriteLine(word);
                 }
-
-                Console.WriteLine(word.Word);
+               
+                
             }
 
             Console.ReadLine();
@@ -40,20 +40,52 @@ namespace Anagram_Finder
             string line;
             while ((line = reader.ReadLine()) != null)
             {
-                Words.Add(new AnagrammizedWord(line.ToLower()));
+                AnnagramBucketSet.Add(line.ToLower());
             }
         }
     }
 
-    public class AnagrammizedWord
+    class AnnagramBucketSet 
     {
-        public string Word { get; set; }
-        public string Anagram { get; set; }
+        private Dictionary<string, AnnagramBucket> BackingDictionary = new Dictionary<string, AnnagramBucket>();
 
-        public AnagrammizedWord(string word)
+        public List<AnnagramBucket> GetTopAnnagramBuckets(int num)
         {
-            this.Word = word;
-            Anagram = String.Concat(word.OrderBy(c => c));
+            return BackingDictionary.Values.OrderByDescending(b => b.AnnagramCount).Take(num).ToList();
+            
+        }
+        public bool Add(string word)
+        {
+            var annagram = String.Concat(word.OrderBy(c => c));
+            if (! BackingDictionary.ContainsKey(annagram))
+            {
+                BackingDictionary[annagram] = new AnnagramBucket(annagram);
+            }
+
+            return BackingDictionary[annagram].Add(word);
+        }
+    }
+
+    public class AnnagramBucket 
+    {
+        public readonly string Annagram;
+        public readonly ISet<string> WordSet = new HashSet<string>();
+
+        public override int GetHashCode()
+        {
+            return Annagram.GetHashCode();
+        }
+
+        public int AnnagramCount => WordSet.Count;
+
+        public AnnagramBucket(string annagram)
+        {
+            Annagram = annagram;
+        }
+
+        public bool Add(string word)
+        {
+            return WordSet.Add(word);
         }
     }
 }
